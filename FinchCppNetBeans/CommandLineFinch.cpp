02dc70cc08d4c2d332f0f84404a4752b2ea4ff4d
Saxon Knight <knight7@hawaii.edu>
2016-05-24 16:54:46 -1000

    Add a simple collision-avoidance routine

--- FinchCppNetBeans/CommandLineFinch.cpp
+++ FinchCppNetBeans/CommandLineFinch.cpp
@@ -29,98 +29,98 @@ void printMenu()
     printf("Q - quit program\n");      
 }
 
-int main(int argc, char* argv[])
-{
-    Finch myFinch;
-    double* accel = 0;
-    int* lightSense = 0;
-    int* obstacles = 0;
-    char option = 0;
-    char trash;
-    double temperature;
-    int leftSpeed, rightSpeed;
-    int rLED, gLED, bLED;
-    int frequency;    
-    int shaken, tapped;
- 
-    printMenu();
-    
-    while(option != 'Q') {
-        printf("Enter desired command: ");
-        scanf("%c",&option);
-        scanf("%c",&trash);
-        switch(option) {
-            case 'A':
-                accel = myFinch.getAccelerations();
-                printf("X: %f, Y: %f, Z: %f\n", accel[0],accel[1],accel[2]);
-                break;
-            case 'o':
-                printf("Level: %d\n", myFinch.isFinchLevel());
-                printf("Beak Up: %d\n", myFinch.isBeakUp());
-                printf("Beak Down: %d\n", myFinch.isBeakDown());
-                printf("Upside Down: %d\n", myFinch.isFinchUpsideDown());
-                printf("Left Wheel Down: %d\n", myFinch.isLeftWingDown());
-                printf("Right Wheel Down: %d\n", myFinch.isRightWingDown());
-                break;
-            case 'L':
-                lightSense = myFinch.getLightSensors();
-                printf("Left: %d, Right: %d\n", lightSense[0],lightSense[1]);
-                break; 
-            case 'I':
-                obstacles = myFinch.getObstacleSensors();
-                printf("Left: %d, Right: %d\n", obstacles[0],obstacles[1]);
-                break;  
-             case 'T':
-                temperature = myFinch.getTemperature();
-                printf("%f Celcius\n", temperature);
-                break;  
-            case 'S':
-                shaken = myFinch.wasShaken();
-                printf("Shaken state: %d\n", shaken);
-                break; 
-            case 't':
-                tapped = myFinch.wasTapped();
-                printf("Tapped state: %d\n", tapped);
-                break;                 
-            case 'B':
-                printf("Enter frequency in Hz: ");
-                scanf("%d", &frequency);
-                scanf("%c",&trash);
-                myFinch.noteOn(frequency);
-                break;
-            case 'b':
-                myFinch.noteOff();
-                break;
-            case 'M':
-                printf("Enter left wheel speed (-255 to 255): ");
-                scanf("%d", &leftSpeed);
-                printf("Enter right wheel speed (-255 to 255): ");
-                scanf("%d", &rightSpeed);
-                scanf("%c",&trash);
-                myFinch.setMotors(leftSpeed, rightSpeed);
-                break;
-            case 'X':
-                myFinch.setMotors(0,0);
-                break;
-            case 'O':
-                printf("Enter red color value (0-255): ");
-                scanf("%d", &rLED);
-                printf("Enter green color value (0-255): ");
-                scanf("%d", &gLED);                
-                printf("Enter blue color value (0-255): ");
-                scanf("%d", &bLED); 
-                scanf("%c",&trash);
-                myFinch.setLED(rLED,gLED,bLED);
-                break;
-            case 'c':
-                printf("%d \n", myFinch.counter());
-                break;
-            case 'm':
-                printMenu();
-                break;
-            default:
-                break;
-            }
-    }
-    return 0;
-}
+//int main(int argc, char* argv[])
+//{
+//    Finch myFinch;
+//    double* accel = 0;
+//    int* lightSense = 0;
+//    int* obstacles = 0;
+//    char option = 0;
+//    char trash;
+//    double temperature;
+//    int leftSpeed, rightSpeed;
+//    int rLED, gLED, bLED;
+//    int frequency;    
+//    int shaken, tapped;
+// 
+//    printMenu();
+//    
+//    while(option != 'Q') {
+//        printf("Enter desired command: ");
+//        scanf("%c",&option);
+//        scanf("%c",&trash);
+//        switch(option) {
+//            case 'A':
+//                accel = myFinch.getAccelerations();
+//                printf("X: %f, Y: %f, Z: %f\n", accel[0],accel[1],accel[2]);
+//                break;
+//            case 'o':
+//                printf("Level: %d\n", myFinch.isFinchLevel());
+//                printf("Beak Up: %d\n", myFinch.isBeakUp());
+//                printf("Beak Down: %d\n", myFinch.isBeakDown());
+//                printf("Upside Down: %d\n", myFinch.isFinchUpsideDown());
+//                printf("Left Wheel Down: %d\n", myFinch.isLeftWingDown());
+//                printf("Right Wheel Down: %d\n", myFinch.isRightWingDown());
+//                break;
+//            case 'L':
+//                lightSense = myFinch.getLightSensors();
+//                printf("Left: %d, Right: %d\n", lightSense[0],lightSense[1]);
+//                break; 
+//            case 'I':
+//                obstacles = myFinch.getObstacleSensors();
+//                printf("Left: %d, Right: %d\n", obstacles[0],obstacles[1]);
+//                break;  
+//             case 'T':
+//                temperature = myFinch.getTemperature();
+//                printf("%f Celcius\n", temperature);
+//                break;  
+//            case 'S':
+//                shaken = myFinch.wasShaken();
+//                printf("Shaken state: %d\n", shaken);
+//                break; 
+//            case 't':
+//                tapped = myFinch.wasTapped();
+//                printf("Tapped state: %d\n", tapped);
+//                break;                 
+//            case 'B':
+//                printf("Enter frequency in Hz: ");
+//                scanf("%d", &frequency);
+//                scanf("%c",&trash);
+//                myFinch.noteOn(frequency);
+//                break;
+//            case 'b':
+//                myFinch.noteOff();
+//                break;
+//            case 'M':
+//                printf("Enter left wheel speed (-255 to 255): ");
+//                scanf("%d", &leftSpeed);
+//                printf("Enter right wheel speed (-255 to 255): ");
+//                scanf("%d", &rightSpeed);
+//                scanf("%c",&trash);
+//                myFinch.setMotors(leftSpeed, rightSpeed);
+//                break;
+//            case 'X':
+//                myFinch.setMotors(0,0);
+//                break;
+//            case 'O':
+//                printf("Enter red color value (0-255): ");
+//                scanf("%d", &rLED);
+//                printf("Enter green color value (0-255): ");
+//                scanf("%d", &gLED);                
+//                printf("Enter blue color value (0-255): ");
+//                scanf("%d", &bLED); 
+//                scanf("%c",&trash);
+//                myFinch.setLED(rLED,gLED,bLED);
+//                break;
+//            case 'c':
+//                printf("%d \n", myFinch.counter());
+//                break;
+//            case 'm':
+//                printMenu();
+//                break;
+//            default:
+//                break;
+//            }
+//    }
+//    return 0;
+//}
--- FinchCppNetBeans/SampleMain.cpp
+++ FinchCppNetBeans/SampleMain.cpp
@@ -6,19 +6,38 @@
  * Created on May 29, 2011, 2:20 PM
  */
 
-/*
+
 #include <cstdlib>
+#include <ctime>
+#include <iostream>
 #include "./Finch/Finch.h"
 
 using namespace std;
 
 int main(int argc, char** argv) {
 
-    Finch myFinch;
-  
-    // Write some code here! 
+    Finch finch;
+    
+    srand(time(NULL));
+    int* ls;
+    do{
+        int* obs=finch.getObstacleSensors();
+        float t3=(rand()%10+1)*100; //Time To Turn = 0.1-1.0 s
+        
+        if(!obs[0]&&!obs[1]){ //Forward is clear
+            finch.setMotors(255,255);
+        }else if(!obs[0]&&obs[1]){ //Obstacle on right
+            finch.setMotors(-255,255,t3); //Turn left
+        }else if(obs[0]&&!obs[1]){ //Obstacle on left
+            finch.setMotors(255,-255,t3); //Turn right
+        }else if(obs[0]&&obs[1]){ //Obstacle dead ahead
+            int lor=rand()%2; //0 or 1 for left or right
+            finch.setMotors(255*lor,255*(1-lor),t3);
+        }
+        
+        ls=finch.getLightSensors();
+        //cout<<ls[0]<<" "<<ls[1]<<endl;
+    }while(ls[0]>50&&ls[1]>50);
 
     return 0;
 }
-
-*/
