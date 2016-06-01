b5d0c75d1a0588ba0a34788c8ef5e37a15b9bdf3
Saxon Knight <knight7@hawaii.edu>
2016-06-01 20:29:53 +0000

    Update comment header to show actual author

--- FinchCppNetBeans/SampleMain.cpp
+++ FinchCppNetBeans/SampleMain.cpp
@@ -1,9 +1,6 @@
 /* 
- * File:   SampleMain.cpp
- * Author: Tom Lauwers (tlauwers@finchrobot.com)
- *
- * Simple skeleton file to show all you need to create a Finch C++ program
- * Created on May 29, 2011, 2:20 PM
+ * Simple collision avoidance
+ * Author: Saxon Knight (knight7@hawaii.edu)
  */
 
 
